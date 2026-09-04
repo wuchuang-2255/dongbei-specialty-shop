9a8adbc2c90e893d0b5c694d1cc2166e219b74d1
wuchuang-2255 <wuchuang42522000@gmail.com>
2026-09-04 19:39:12 +0800

    Fix black fungus image and add 20 products

--- src/main/java/cn/edu/demo/shop/ProductRepository.java
+++ src/main/java/cn/edu/demo/shop/ProductRepository.java
@@ -1,3 +1,3 @@
 package cn.edu.demo.shop;
-import org.springframework.data.jpa.repository.JpaRepository; import java.util.List;
-public interface ProductRepository extends JpaRepository<Product,Long>{ List<Product> findByNameContainingIgnoreCaseOrCategoryContainingIgnoreCase(String name,String category); }
+import org.springframework.data.jpa.repository.JpaRepository; import java.util.List; import java.util.Optional;
+public interface ProductRepository extends JpaRepository<Product,Long>{ List<Product> findByNameContainingIgnoreCaseOrCategoryContainingIgnoreCase(String name,String category); boolean existsByName(String name); Optional<Product> findByName(String name); }
